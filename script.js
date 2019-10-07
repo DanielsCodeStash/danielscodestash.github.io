3a25aa57dbd8b17ccfea2d35e4daeff79c9f930c
DanielsCodeStash <->
2019-10-07 20:27:26 +0200

    add external file data load

--- script.js
+++ script.js
@@ -3,49 +3,19 @@ window.app = {};
 
 window.onload = function(){
 
-    var data = [
-        { g1: "Bostad", g2: "Amortering", value: 3125 },
-        { g1: "Bostad", g2: "Ränta", value: 2092 },
-        { g1: "Bostad", g2: "Hyra", value: 2618 },
-        { g1: "Bostad", g2: "Vatten", value: 200 },
-        { g1: "Bostad", g2: "El", value: 325 },
-    
-        { g1: "Fasta utgift", g2: "Tandvård", value: 98 },
-        { g1: "Fasta utgift", g2: "Akassa", value: 110 },
-        { g1: "Fasta utgift", g2: "CSN", value: 594 },
-        { g1: "Fasta utgift", g2: "Försäkring", value: 98 },
-        { g1: "Fasta utgift", g2: "Linser", value: 350 },
-        { g1: "Fasta utgift", g2: "Telfon", value: 99 },
-        { g1: "Fasta utgift", g2: "Bank", value: 35 },
-        { g1: "Fasta utgift", g2: "Streaming", value: 350 },
-    
-        { g1: "Base", g2: "Mat och bas", value: 5456 },
-        { g1: "Base", g2: "Öl", value: 1194 },
-    
-        { g1: "One time", g2: "Bulk tandvård", value: 1600 },
-        { g1: "One time", g2: "Plantering LED", value: 750 },
-        { g1: "One time", g2: "Hörlurar", value: 900 },
-        { g1: "One time", g2: "Lödning/Multimeter", value: 600 },
-        { g1: "One time", g2: "Kassaskåp", value: 500 },
-        { g1: "One time", g2: "Datorspel", value: 400 },
-    
-    
-        { g1: "Ingrid", g2: "Gym", value: 399 },
-        { g1: "Ingrid", g2: "Telefon", value: 99 },
-        { g1: "Ingrid", g2: "Spotify", value: 49 },
-    
-        { g1: "Sparande", g2: "Sparande", value: 6500 },
-      ];
-    
+    $.getJSON("data/201909.json", function(json) {
+
       window.app.treemap = new d3plus.Treemap()
-        .data(data)
-        .groupBy(["g1", "g2"])
-        .select("#graphPanel")
-        .sum("value")
-        .render();     
+      .data(json)
+      .groupBy(["g1", "g2"])
+      .select("#graphPanel")
+      .sum("value")
+      .render();
+
+      window.app.resizeChart();
+  });    
+  
 }
-
-
 
 window.app.resizeChart = function() {
     var w = window.innerWidth;
@@ -54,13 +24,14 @@ window.app.resizeChart = function() {
     document.getElementById('topPanel').setAttribute("style","height: " + 60 + "px");
     document.getElementById('mainPanel').setAttribute("style","padding-left:10px");
 
-    window.app.treemap.width(w-20);
-    window.app.treemap.height(h-60-50);
+    if(window.app.treemap) {
+      window.app.treemap.width(w-20);
+      window.app.treemap.height(h-60-50);
 
-    window.app.treemap.render();
+      window.app.treemap.render();
+    }
     console.log("w = " + w + ", h = " + h)
 }
-
 
 window.addEventListener('load', function(event){
     window.app.resizeChart();
